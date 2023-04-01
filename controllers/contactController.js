bf3201fa0c522b884f816517cda459ad096d247e
Rafael Armenta <rarmenta@hawk.iit.edu>
2023-04-01 02:54:15 -0500

    adjusted controller to work with MongoDB

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,11 @@
-const contactsRepo = require('../src/contactsSQLiteRepository');
+const contactsRepo = require('../src/contactsMongoRepository');
 const { validationResult } = require('express-validator');
 const Contact = require('../src/Contact');
 
 
 /* GET users listing. */
-exports.contacts_list = function(req, res, next) {
-  const data = contactsRepo.findAll();
+exports.contacts_list = async function(req, res, next) {
+  const data = await contactsRepo.findAll();
   res.render('contacts', {title: 'Express Contacts', contacts: data });
 };
 
@@ -15,22 +15,22 @@ exports.contacts_create_get = function(req, res, next) {
 };
 
 /* POST create contacts. */
-exports.contacts_create_post = function(req, res, next) {
+exports.contacts_create_post = async function(req, res, next) {
   //console.log(req.body);
   const result = validationResult(req);
   if (!result.isEmpty())  {
       res.render('contacts_add', { title: 'Add a contact', msg: result.array()});
   } else  {
     const newContact = new Contact('', req.body.firstName, req.body.lastName, req.body.email, req.body.notes);
-    contactsRepo.create(newContact)
+    await contactsRepo.create(newContact)
 
     res.redirect('/contacts');
   }
 };
 
 /* GET single contacts. */
-exports.contacts_detail = function(req, res, next) {
-  const contact = contactsRepo.findById(req.params.uuid);
+exports.contacts_detail = async function(req, res, next) {
+  const contact = await contactsRepo.findById(req.params.uuid);
   if(contact) {
     res.render('contact', {title: 'Contact Information', contact: contact });
   } else {
@@ -39,33 +39,33 @@ exports.contacts_detail = function(req, res, next) {
 };
 
 /* GET delete contacts form. */
-exports.contacts_delete_get = function(req, res, next) {
-  const contact = contactsRepo.findById(req.params.uuid);
+exports.contacts_delete_get = async function(req, res, next) {
+  const contact = await contactsRepo.findById(req.params.uuid);
   res.render('contacts_delete', {title: 'Delete Contact', contact: contact });
 };
 
 /* POST delete contacts. */
-exports.contacts_delete_post = function(req, res, next) {
-  contactsRepo.deleteByID(req.params.uuid);
+exports.contacts_delete_post = async function(req, res, next) {
+  await contactsRepo.deleteByID(req.params.uuid);
   res.redirect('/contacts');
 };
 
 /* GET edit contacts form. */
-exports.contacts_edit_get = function(req, res, next) {
-  const contact = contactsRepo.findById(req.params.uuid);
+exports.contacts_edit_get = async function(req, res, next) {
+  const contact = await contactsRepo.findById(req.params.uuid);
   res.render('contacts_edit', {title: 'Edit contact', contact: contact });
 };
 
 /* POST edit contacts. */
-exports.contacts_edit_post = function(req, res, next) {
+exports.contacts_edit_post = async function(req, res, next) {
   //console.log(req.body);
   const result = validationResult(req);
   if (!result.isEmpty())  {
-    const contact = contactsRepo.findById(req.params.uuid);
+    const contact = await contactsRepo.findById(req.params.uuid);
     res.render('contacts_edit', { title: 'Edit contact', msg: result.array(), contact: contact});
   } else  {
     const updatedContact = new Contact(req.params.uuid, req.body.firstName, req.body.lastName, req.body.email, req.body.notes, req.body.notes );
-    contactsRepo.update(updatedContact);
+    await contactsRepo.update(updatedContact);
     res.redirect('/contacts/' + req.params.uuid);
   }
 };
